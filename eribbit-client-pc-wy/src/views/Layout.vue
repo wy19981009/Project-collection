d6f34f2d7f7f49fab948c4fff2c7390871f8c98a
王洋 <8869685+wang-yang-WYYXL@user.noreply.gitee.com>
2022-10-11 15:22:49 +0800

    底部布局

--- eribbit-client-pc-wy/src/views/Layout.vue
+++ eribbit-client-pc-wy/src/views/Layout.vue
@@ -3,19 +3,26 @@
   <header>
     <AppHeader/>
   </header>
-  <main>
+  <main class="app-body">
     <!-- 二级路由 -->
     <router-view></router-view>
   </main>
-    <footer>底部</footer>
+    <footer>
+      <AppFooter/>
+    </footer>
   </template>
 <script>
 import AppNavbar from '@/components/app-navbar.vue'
 import AppHeader from '../components/app-header.vue'
+import AppFooter from '../components/app-footer.vue'
 
 export default {
   name: 'Layout',
-  components: { AppNavbar, AppHeader }
+  components: { AppNavbar, AppHeader, AppFooter }
 }
 </script>
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.app-body {
+  height: 600px;
+}
+</style>
